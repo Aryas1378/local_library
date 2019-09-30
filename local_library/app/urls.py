21df2a14f848107e92c64278a75000f5df2f0125
Aryas1378 <aryas1378@gmail.com>
2019-09-30 00:33:18 -0700

    changing in url and setting files

---
+++ local_library/app/urls.py
@@ -0,0 +1,4 @@
+from django.URLconf.urls import url
+urlpatterns = [
+    
+]
--- local_library/local_library/urls.py
+++ local_library/local_library/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import setting, include
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app/', include('app.urls'))
 ]
+
+urlpatterns += static(setting.STATIC_URL, document_root=setting.STATIC_ROOT)
